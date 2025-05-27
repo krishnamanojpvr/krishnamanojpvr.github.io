69c14e642fff9dd7de8471670e6d1c4ab9ee939e
Krishna Manoj PVR <119659348+krishnamanojpvr@users.noreply.github.com>
2025-05-27 10:37:44 +0530

    Create build.fish

---
+++ .github/build.fish
@@ -0,0 +1,40 @@
+#!/usr/bin/env fish
+
+set subdomain manojliveme 
+
+function buildRepo
+    set repo_url "https://github.com/krishnamanojpvr/manojliveme.git"
+    set repo_dir manojliveme
+
+    if test ! -e site
+        mkdir site
+    else if test -e ./site/$subdomain
+        rm -rf ./site/$subdomain
+    end
+
+    if test ! -e buildfiles
+        mkdir buildfiles
+    else
+        rm -rf ./buildfiles/*
+    end
+
+    echo "Cloning $repo_url"
+    set auth_repo_url (string replace "https://" "https://$PA_TOKEN:@" $repo_url)
+    git clone "$auth_repo_url" "$repo_dir"
+
+    cd $repo_dir
+    bun i
+    bun run build
+    cd -
+
+    mkdir -p "./site/$subdomain"
+    if test -e $repo_dir/build
+        cp -r $repo_dir/build/* ./site/$subdomain
+    else if test -e $repo_dir/dist
+        cp -r $repo_dir/dist/* ./site/$subdomain
+    end
+
+    rm -rf $repo_dir
+end
+
+buildRepo
